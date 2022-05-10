6c5fc7649e20d4f5329bf2196483d88e1623e64b
shtanmoy <tanmoysharma46@gmail.com>
2022-05-10 17:28:41 +0600

    5:28pm

--- peopole.js
+++ peopole.js
@@ -2,6 +2,9 @@ const people = ["james", "Helon", "jabis"];
 const a = 5;
 function test() {
   console.log("this is a function");
+  console.log(module);
+  console.log(__dirname);
+  console.log(__filename);
 }
 
 module.exports={people,a,test2:test};
